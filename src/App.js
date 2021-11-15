f18877dfd643cd560ad3b6b72f02e44224615ac5
virajdalvi <viraj512dalvi@gmail.com>
2021-11-15 19:32:19 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Coin from "./Components/Coin";
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log("error"));
+  });
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+  const filterCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="liveCrypto">
+      <div className="coinSearch">
+        <h1 className="searchText">Live Crypto</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Search Your Coin...."
+            className="searchInput"
+            onChange={handleChange}
+          />
+        </form>
+      </div>
+      {filterCoins.map((coin) => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            volume={coin.total_volume}
+          />
+        );
+      })}
     </div>
   );
 }
+
 export default App;
